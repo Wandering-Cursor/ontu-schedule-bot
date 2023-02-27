947824d526e9a00011900e82b5c180cc8d161b8c
Pavlo Pohorieltsev <667strets@gmail.com>
2023-02-27 08:04:31 +0200

    Fix notifications

--- ontu_schedule_bot/decorators.py
+++ ontu_schedule_bot/decorators.py
@@ -2,28 +2,34 @@
 from telegram import Update
 
 
+def _print(exception: Exception):
+    """Print exception for methods"""
+    print(f"Виникла помилка:\n{exception.args}")
+    return None
+
+
 def reply_with_exception(func):
     """
     If while processing a call ValueError occurs - tries to reply with text to a message
     """
-    async def inner(update: Update, *args, **kwargs):
+    async def inner(*args, update: Update | None = None, **kwargs):
         try:
-            value = await func(update, *args, **kwargs)
+            value = await func(update=update, *args, **kwargs)
             return value
         except ValueError as exception:
+            if not update:
+                return _print(exception)
+
             query = update.callback_query
             message = None
+
             if update.message:
                 message = update.message
             if query and query.message:
                 message = query.message
 
             if not message:
-                print(
-                    f"Виникла помилка:\n{exception.args}"
-                )
-                return None
-
+                return _print(exception)
             await message.reply_text(
                 f"Виникла помилка:\n{exception.args}"
             )
