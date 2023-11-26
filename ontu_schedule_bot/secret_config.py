0a51a35a8487e278db655d3e921f7c3664f72245
Pavlo Pohorieltsev <667strets@gmail.com>
2023-11-26 14:19:54 +0200

    Fix config

--- ontu_schedule_bot/secret_config.py
+++ ontu_schedule_bot/secret_config.py
@@ -18,9 +18,9 @@ if not API_URL:
     print("Saved API_URL to .env")
 
 DEBUG_CHAT_ID = config("DEBUG_CHAT_ID", default=None)
-if not API_URL:
+if not DEBUG_CHAT_ID:
     print("DEBUG_CHAT_ID not found in .env")
-    API_URL = input("Enter DEBUG_CHAT_ID to receive error messages: ")
+    DEBUG_CHAT_ID = input("Enter DEBUG_CHAT_ID to receive error messages: ")
     with open(".env", "a+", encoding="UTF-8") as file:
         file.write(f"DEBUG_CHAT_ID={DEBUG_CHAT_ID}\n")
     print("Saved DEBUG_CHAT_ID to .env")
