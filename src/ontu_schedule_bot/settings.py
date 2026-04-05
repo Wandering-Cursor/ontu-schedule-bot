bc9c2f2320af8bb6c01911d14a3a331f142c3718
Pavlo Pohorieltsev <49622129+makisukurisu@users.noreply.github.com>
2026-04-05 20:19:40 +0000

    revert debug change

--- src/ontu_schedule_bot/settings.py
+++ src/ontu_schedule_bot/settings.py
@@ -29,7 +29,7 @@ class Settings(BaseSettings):
 
     WEBHOOK_URL: pydantic.HttpUrl | None = None
     RUN_PERIODIC_JOBS: bool = True
-    DEBUG: bool = True
+    DEBUG: bool = False
 
 
 settings = Settings()  # pyright: ignore[reportCallIssue]
